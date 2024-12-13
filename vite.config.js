b2aef41b8d30840154157459ff261093285fc0cf
Shubham Raj <29100993+shubham14p3@users.noreply.github.com>
2024-12-13 18:28:22 +0530

    Adding static ip fpr ui

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Bind to all network interfaces
+    host: '34.200.90.9', // Bind to all network interfaces
     port: 5173      // Optional: Customize the port if needed
   },
 })
